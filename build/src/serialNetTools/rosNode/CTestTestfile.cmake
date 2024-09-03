034e611a4f0f2773e4c97261bbd145ff389456fa
Leeve <libu316@nuaa.edu.cn>
2024-09-03 16:32:26 +0800

    [添加gitignore文件] 重新构建主分支，合并develop分支

--- build/src/serialNetTools/rosNode/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/u/Desktop/cmakeproject/src/serialNetTools/rosNode
-# Build directory: /home/u/Desktop/cmakeproject/build/src/serialNetTools/rosNode
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../../../gtest")
