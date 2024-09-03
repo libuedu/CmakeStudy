ba12a54b4dd6f535dab2a5cd78cd86556a99c74a
Leeve <libu316@nuaa.edu.cn>
2024-09-03 21:02:51 +0800

    [复习项目框架] 通过sudo socat -d -d pty,raw,echo=0 pty,raw,echo=0创建虚拟串口，通过连接创建好的虚拟串口进行通讯，

--- src/serialNetTools/mySerial/main_test.cpp
+++ src/serialNetTools/mySerial/main_test.cpp
@@ -3,6 +3,7 @@
 #include "myLog.h"
 #include "common.h"
 #include <QCoreApplication>
+#include <QThread>
 
 int main(int argc, char **argv)
 {
@@ -15,7 +16,16 @@ int main(int argc, char **argv)
     QCoreApplication app(argc, argv);
 
     MySerial *mySerial = new MySerial("1", "/dev/pts/5", 115200, 0, 8, 1);
-    QMetaObject::invokeMethod(mySerial, "slot_start", Qt::QueuedConnection);
+    // QMetaObject::invokeMethod(mySerial, "slot_start", Qt::QueuedConnection);
 
-    return app.exec();
+    QThread *thread = new QThread();
+    mySerial->moveToThread(thread);
+    QObject::connect(thread, &QThread::started, mySerial, &MySerial::slot_start);
+    thread->start();
+    int result = app.exec();
+
+    mySerial->deleteLater();
+    thread->deleteLater();
+
+    return result;
 }
--- src/serialNetTools/rosNode/main_test.cpp
+++ src/serialNetTools/rosNode/main_test.cpp
@@ -25,5 +25,5 @@ int main(int argc, char **argv)
 
     sleep(1);
 
-    return 0;
+    return result;
 }
